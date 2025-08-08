135f352f43620cc636d6cb2798fd821d69b8cfa7
dimaspreset <dimaspreset@gmail.com>
2025-08-08 14:36:49 +0800

    add notif listener

--- notification_listener.py
+++ notification_listener.py
@@ -1,8 +1,16 @@
 from jnius import autoclass, PythonJavaClass, java_method
-from android.runnable import run_on_ui_thread
+from kivy.utils import platform
 
 # Variabel global untuk menyimpan callback
 notification_callback = None
+
+# Cek apakah sedang dijalankan di Android
+if platform == 'android':
+    from android.runnable import run_on_ui_thread
+else:
+    # Dummy dekorator untuk non-Android (agar tidak error saat build)
+    def run_on_ui_thread(func):
+        return func
 
 # Kelas Python untuk menerima notifikasi dari Java
 class NotificationReceiver(PythonJavaClass):
@@ -24,6 +32,10 @@ def set_notification_callback(callback):
 # Fungsi untuk inisialisasi listener dari sisi Python
 @run_on_ui_thread
 def start_notification_listener():
+    if platform != 'android':
+        print("Notification listener hanya berjalan di Android.")
+        return
+
     PythonActivity = autoclass('org.kivy.android.PythonActivity')
     activity = PythonActivity.mActivity
 
